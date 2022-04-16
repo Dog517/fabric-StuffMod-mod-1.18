26a5cc1e8978ddae1f79934860defd3e237680c5
Admin <home@Abbis-Quad-Core-i5-iMac.local>
2022-04-16 12:08:30 -0400

    Initial commit

---
+++ src/main/java/net/dogdev/stuffmod/painting/ModPaintings.java
@@ -0,0 +1,26 @@
+package net.dogdev.stuffmod.painting;
+
+import net.dogdev.stuffmod.StuffMod;
+import net.fabricmc.fabric.api.networking.v1.S2CPlayChannelEvents;
+import net.minecraft.entity.decoration.painting.PaintingMotive;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModPaintings {
+
+    public static final PaintingMotive RICK = RegisterPaintings("rick", new PaintingMotive(16,16));
+
+
+
+
+        private static PaintingMotive RegisterPaintings(String name, PaintingMotive paintingMotive){
+            return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(StuffMod.MOD_ID, name), paintingMotive);
+        }
+
+
+    public static void registerPainting(){
+
+        StuffMod.LOGGER.info("Registering Paintings For " + StuffMod.MOD_ID);
+
+    }
+}
